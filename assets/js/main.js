a269291d4701b8880a1637ea33cfb66c5ba1fd25
khimsh <i.khimsh@gmail.com>
2019-10-14 23:33:44 +0400

    API year parameter is no longer hardcoded.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 const holidayStatus = document.getElementById('holidayStatus');
 
+const currentYear = new Date().getFullYear();
 const api_key = '95ff1549485f8e4c6ffa1d486d4ab7438dfb3688';
-const url = `https://calendarific.com/api/v2/holidays?&api_key=${api_key}&country=ge&year=2019`;
+const url = `https://calendarific.com/api/v2/holidays?&api_key=${api_key}&country=ge&year=${currentYear}`;
 
 async function getHolidays(url) {
   let response = await fetch(url);
