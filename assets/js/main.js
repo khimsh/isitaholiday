29b28024ece72b46e5602f006c6320ee32fd6b4e
khimsh <i.khimsh@gmail.com>
2019-10-14 22:59:44 +0400

    Initial commit - basic functionality.

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+const holidayStatus = document.getElementById('holidayStatus');
+
+const api_key = '95ff1549485f8e4c6ffa1d486d4ab7438dfb3688';
+const url = `https://calendarific.com/api/v2/holidays?&api_key=${api_key}&country=ge&year=2019`;
+
+async function getHolidays(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+getHolidays(url)
+  .then(data => {
+    // console.log(data);
+    return data.response.holidays;
+  })
+  .then(holidays => {
+    let result = holidays.find(holiday => holiday.date.iso === getIsoDate());
+    if (result) {
+      result.type.includes('National holiday');
+      holidayStatus.innerText = 'Yesss';
+    } else {
+      holidayStatus.innerText = 'No :(';
+    }
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const getIsoDate = () => {
+  const date = new Date();
+  const date_iso = date.toISOString().slice(0, 10);
+  return date_iso;
+};
